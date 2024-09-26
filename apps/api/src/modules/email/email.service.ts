e36e2ab9837e3f611f3c03dc59735cd3548bcd15
Zain Zafar <zainzafar90@gmail.com>
2024-09-26 17:30:54 +0500

    refactor(email-service): update email URLs
    - Update the email service to use a dynamic client URL based on the environment.
    - Replace hardcoded URLs with dynamically generated URLs using the client URL from the configuration.

--- apps/api/src/modules/email/email.service.ts
+++ apps/api/src/modules/email/email.service.ts
@@ -52,8 +52,12 @@ export const sendResetPasswordEmail = async (
   token: string
 ): Promise<void> => {
   const subject = "Reset password";
+  const clientUrl =
+    config.env === "production"
+      ? config.clientUrl
+      : `http://${config.clientUrl}`;
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://${config.clientUrl}/reset-password?token=${token}`;
+  const resetPasswordUrl = `http://${clientUrl}/reset-password?token=${token}`;
   const text = `Hi,
   To reset your password, click on this link: ${resetPasswordUrl}
   If you did not request any password resets, then ignore this email.`;
@@ -78,8 +82,12 @@ export const sendVerificationEmail = async (
   name: string
 ): Promise<void> => {
   const subject = "Email Verification";
+  const clientUrl =
+    config.env === "production"
+      ? config.clientUrl
+      : `http://${config.clientUrl}`;
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://${config.clientUrl}/verify-email?token=${token}`;
+  const verificationEmailUrl = `http://${clientUrl}/verify-email?token=${token}`;
   const text = `Hi ${name},
   To verify your email, click on this link: ${verificationEmailUrl}
   If you did not create an account, then ignore this email.`;
@@ -156,8 +164,12 @@ export const sendSuccessfulRegistration = async (
   name: string
 ): Promise<void> => {
   const subject = "Email Verification";
+  const clientUrl =
+    config.env === "production"
+      ? config.clientUrl
+      : `http://${config.clientUrl}`;
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://${config.clientUrl}/verify-email?token=${token}`;
+  const verificationEmailUrl = `http://${clientUrl}/verify-email?token=${token}`;
   const text = `Hi ${name},
   Congratulations! Your account has been created. 
   You are almost there. Complete the final step by verifying your email at: ${verificationEmailUrl}
@@ -184,8 +196,12 @@ export const sendAccountCreated = async (
   name: string
 ): Promise<void> => {
   const subject = "Account Created Successfully";
+  const clientUrl =
+    config.env === "production"
+      ? config.clientUrl
+      : `http://${config.clientUrl}`;
   // replace this url with the link to the email verification page of your front-end app
-  const loginUrl = `http://${config.clientUrl}/auth/login`;
+  const loginUrl = `http://${clientUrl}/auth/login`;
   const text = `Hi ${name},
   Congratulations! Your account has been created successfully. 
   You can now login at: ${loginUrl}
