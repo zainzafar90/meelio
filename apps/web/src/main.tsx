b776ae53cbe8b388bb28ececcf031c8f46d10987
Zain Zafar <zainzafar90@gmail.com>
2024-10-21 02:50:25 +0500

    Refactor main.tsx: Add automatic service worker update

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,7 +7,30 @@ import { App } from "./app";
 
 import "@/styles/globals.css";
 
-registerSW();
+const INTERVAL_MS = 60 * 60 * 1000; // 1 hour
+
+const updateSW = registerSW({
+  onRegisteredSW(swUrl, r) {
+    r &&
+      setInterval(async () => {
+        if (r.installing || !navigator) return;
+
+        if ("connection" in navigator && !navigator.onLine) return;
+
+        const resp = await fetch(swUrl, {
+          cache: "no-store",
+          headers: {
+            cache: "no-store",
+            "cache-control": "no-cache",
+          },
+        });
+
+        if (resp?.status === 200) await r.update();
+      }, INTERVAL_MS);
+  },
+});
+
+updateSW();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
