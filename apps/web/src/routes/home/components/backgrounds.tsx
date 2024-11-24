064d2391119a166c4d527093aee09dc14e0c7574
Zain Zafar <zainzafar90@gmail.com>
2024-11-25 03:44:12 +0500

    use dynamic background

--- apps/web/src/routes/home/components/backgrounds.tsx
+++ apps/web/src/routes/home/components/backgrounds.tsx
@@ -11,7 +11,8 @@ export const Background = () => {
       <div
         className="absolute inset-0 bg-transparent bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('./img/bg-08.webp')",
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1505699261378-c372af38134c?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         }}
       />
     </div>
