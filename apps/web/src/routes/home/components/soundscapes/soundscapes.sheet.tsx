79639dca9b8618f6d84de0bedf692521f168f6bb
Zain Zafar <zainzafar90@gmail.com>
2024-11-25 03:03:10 +0500

    use background image

--- apps/web/src/routes/home/components/soundscapes/soundscapes.sheet.tsx
+++ apps/web/src/routes/home/components/soundscapes/soundscapes.sheet.tsx
@@ -31,6 +31,15 @@ export const SoundscapesSheet: React.FC = () => {
         side="right"
         className="flex w-full flex-col gap-0 p-0 sm:max-w-xl"
       >
+        <div
+          className="absolute inset-0 bg-black opacity-10"
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1577730286200-046d1c45439a?q=80&w=2827&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
         <VisuallyHidden>
           <SheetHeader>
             <SheetTitle>{t("soundscapes.dialog.title")}</SheetTitle>
