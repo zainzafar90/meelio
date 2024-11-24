3caddbebf30f2863379f75f5cbe28cb19cc2ac87
Zain Zafar <zainzafar90@gmail.com>
2024-11-25 03:09:17 +0500

    update wallpaper

--- apps/web/src/routes/home/components/soundscapes/soundscapes.sheet.tsx
+++ apps/web/src/routes/home/components/soundscapes/soundscapes.sheet.tsx
@@ -35,7 +35,7 @@ export const SoundscapesSheet: React.FC = () => {
           className="absolute inset-0 bg-black opacity-10"
           style={{
             backgroundImage:
-              "url(https://images.unsplash.com/photo-1577730286200-046d1c45439a?q=80&w=2827&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+              "url(https://images.unsplash.com/photo-1569935801172-5b458e214ee8?q=80&w=720&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
