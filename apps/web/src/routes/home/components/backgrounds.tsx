7d99af543011e26fc5fb8d115ff8cf8f65ddadb7
Zain Zafar <zainzafar90@gmail.com>
2024-11-25 03:48:15 +0500

    improve: remove overlay

--- apps/web/src/routes/home/components/backgrounds.tsx
+++ apps/web/src/routes/home/components/backgrounds.tsx
@@ -12,7 +12,7 @@ export const Background = () => {
         className="absolute inset-0 bg-transparent bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1505699261378-c372af38134c?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+            "url('https://images.unsplash.com/photo-1505699261378-c372af38134c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         }}
       />
     </div>
@@ -23,12 +23,9 @@ export const BackgroundOverlay = () => {
   return (
     <div
       className={cn(
-        "fixed inset-0",
+        "fixed inset-0 bg-black/5",
         "transition-transform duration-300 ease-out"
       )}
-      style={{
-        backgroundImage: "url('./img/overlay.png')",
-      }}
     />
   );
 };
