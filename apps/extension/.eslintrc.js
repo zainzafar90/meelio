c56449baf1be2fb06587435c62f242aefbc7211d
Zain Zafar <zainzafar90@gmail.com>
2024-11-30 17:24:58 +0500

    update configs plasmo

--- apps/extension/.eslintrc.js
+++ apps/extension/.eslintrc.js
@@ -1,23 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  extends: ["@repo/eslint-config/library"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-  },
-  parserOptions: {
-    project: true,
   },
 };
