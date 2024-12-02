cad1b421eaccb606415bdccbed6c7f261543e951
Zain Zafar <zainzafar90@gmail.com>
2024-12-02 16:33:27 +0500

    refactor: reuse postcss configs for project

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,2 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// @ts-expect-error - No types for postcss
+module.exports = require("@repo/tailwind-config/postcss.config");
