5d37d02414ce2fdc281d29dd561ef6ae8ce9f5c5
Zain Zafar <zainzafar90@gmail.com>
2025-02-25 12:47:12 +0500

    style: Remove hardcoded background image from soundscapes sheet

--- packages/shared/src/components/core/soundscapes/soundscapes.sheet.tsx
+++ packages/shared/src/components/core/soundscapes/soundscapes.sheet.tsx
@@ -30,15 +30,7 @@ export const SoundscapesSheet: React.FC = () => {
         side="right"
         className="flex w-full flex-col gap-0 p-0 sm:max-w-xl"
       >
-        <div
-          className="absolute inset-0 bg-black opacity-10"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1569935801172-5b458e214ee8?q=80&w=720&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
+        <div className="absolute inset-0 bg-black opacity-10" />
         <VisuallyHidden>
           <SheetHeader>
             <SheetTitle>{t("soundscapes.dialog.title")}</SheetTitle>
