406413e9a99a4cd0f12261461d94969a78d92579
Zain Zafar <zainzafar90@gmail.com>
2025-03-13 04:48:47 +0500

    feat: Add guest user functionality to AuthContext with placeholder methods

--- packages/shared/src/context/auth-context.tsx
+++ packages/shared/src/context/auth-context.tsx
@@ -4,6 +4,8 @@ import React from "react";
 export const AuthContext = React.createContext<AuthState>({
   loading: false,
   user: null,
+  guestUser: null,
   authenticate: () => console.warn("NOOP"),
+  authenticateGuest: () => console.warn("NOOP"),
   logout: () => console.warn("NOOP"),
 });
