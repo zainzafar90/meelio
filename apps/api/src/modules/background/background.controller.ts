a945142cfe984c4c9e40cbc840a688c4bfb44c24
Zain Zafar <zainzafar90@gmail.com>
2025-05-21 00:49:06 +0500

    refactor: Simplify background controller and service by removing unused code and default backgrounds, returning success responses for background operations

--- apps/api/src/modules/background/background.controller.ts
+++ apps/api/src/modules/background/background.controller.ts
@@ -1,26 +1,21 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { catchAsync } from "@/utils/catch-async";
-import { backgroundService } from "./background.service";
-import { IUser } from "@/types/interfaces/resources";
 
 export const backgroundController = {
   /**
    * Get all backgrounds for the authenticated user
    */
   getBackgrounds: catchAsync(async (req: Request, res: Response) => {
-    const user = req.user as IUser;
-
-    const backgrounds = await backgroundService.getBackgrounds(user.id);
-    return res.status(httpStatus.OK).json(backgrounds);
+    return res.status(httpStatus.OK).json({
+      success: true,
+    });
   }),
 
   /**
    * Set a background as selected for the authenticated user
    */
   setFavouriteBackground: catchAsync(async (req: Request, res: Response) => {
-    const user = req.user as IUser;
-
     const { backgroundId } = req.body;
     if (!backgroundId) {
       return res.status(httpStatus.BAD_REQUEST).json({
@@ -28,16 +23,8 @@ export const backgroundController = {
       });
     }
 
-    const selectedBackground = await backgroundService.setFavouriteBackground(
-      user.id,
-      backgroundId
-    );
-
-    const backgrounds = await backgroundService.getBackgrounds(user.id);
-
     return res.status(httpStatus.OK).json({
-      backgrounds,
-      selectedBackground,
+      success: true,
     });
   }),
 };
