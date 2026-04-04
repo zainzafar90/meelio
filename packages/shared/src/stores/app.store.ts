01c1a058e45436b57b4958b300e72742243be6a8
Zain Zafar <zainzafar90@gmail.com>
2026-04-04 18:49:12 +0500

    chore: bump version to 0.9.4

--- packages/shared/src/stores/app.store.ts
+++ packages/shared/src/stores/app.store.ts
@@ -23,7 +23,7 @@ interface AppState {
 export const useAppStore = create<AppState>()(
   persist(
     (set) => ({
-      version: "0.9.3",
+      version: "0.9.4",
       platform: "extension",
       mantraRotationCount: 0,
       mantraRotationEnabled: true,
